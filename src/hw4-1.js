41274e64989f41d588dd5cb7c67c073785deddba
Ilnur Gaysin <ilnur.gaysin@rt.ru>
2021-07-29 23:25:09 +0300

    add methods: sumSymbols and sumDigitNumber

--- src/hw4-1.js
+++ src/hw4-1.js
@@ -1,3 +1,15 @@
 export function mult(a, b) {
     return a * b;
 }
+
+export function sumSymbols(a, b) {
+    return a.length + b.length;
+}
+
+export function sumDigitNumber(a) {
+    return a
+        .toString()
+        .split('')
+        .map(Number)
+        .reduce((acc, cur) => acc + cur)
+}
--- src/hw4-1.test.js
+++ src/hw4-1.test.js
@@ -1,7 +1,21 @@
 import { mult } from "./hw4-1";
+import { sumSymbols } from "./hw4-1";
+import { sumDigitNumber } from "./hw4-1";
 
-describe("mult", () => {
+describe("multiplication", () => {
     it("should work correct", () => {
         expect(mult(2, 3)).toBe(6);
+    });
+});
+
+describe("sumSymbols", () => {
+    it("should work correct", () => {
+        expect(sumSymbols('one', 'five')).toBe(7);
+    });
+});
+
+describe("sumDigitNumber", () => {
+    it("should work correct", () => {
+        expect(sumDigitNumber(345)).toBe(12);
     });
 });
